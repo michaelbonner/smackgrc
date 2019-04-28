79498313523f9304bf7d31f89affecfb2cef91d0
Michael Bonner <michaelwbonner@gmail.com>
2019-04-28 17:46:35 -0600

    more layout updates

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,115 +1,120 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import React from "react";
+import { Link } from "gatsby";
 
-import logo from '../img/logo-light.svg'
-import facebook from '../img/social/facebook.svg'
-import instagram from '../img/social/instagram.svg'
-import twitter from '../img/social/twitter.svg'
-import vimeo from '../img/social/vimeo.svg'
+import logo from "../img/logo-light.svg";
+import facebook from "../img/social/facebook.svg";
+import instagram from "../img/social/instagram.svg";
+import twitter from "../img/social/twitter.svg";
+import linkedin from "../img/social/linkedin.svg";
 
 const Footer = class extends React.Component {
   render() {
     return (
-      <footer className="footer has-background-black has-text-white-ter">
-        <div className="content text-center">
-          <img
-            src={logo}
-            alt="SMACK GRC"
-            style={{ width: '14em', height: '10em' }}
-          />
-        </div>
-        <div className="content text-center has-background-black has-text-white-ter">
-          <div className="container has-background-black has-text-white-ter">
-            <div className="columns">
-              <div className="column is-4">
-                <section className="menu">
-                  <ul className="menu-list">
-                    <li>
-                      <Link to="/" className="navbar-item">
-                        Home
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/about">
-                        About
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/products">
-                        Products
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/contact/examples">
-                        Form Examples
-                      </Link>
-                    </li>
-                    <li>
-                      <a
-                        className="navbar-item"
-                        href="/admin/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        Admin
-                      </a>
-                    </li>
-                  </ul>
-                </section>
-              </div>
-              <div className="column is-4">
-                <section>
-                  <ul className="menu-list">
-                    <li>
-                      <Link className="navbar-item" to="/blog">
-                        Latest Stories
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/contact">
-                        Contact
-                      </Link>
-                    </li>
-                  </ul>
-                </section>
-              </div>
-              <div className="column is-4 social">
-                <a title="facebook" href="https://facebook.com">
-                  <img
-                    src={facebook}
-                    alt="Facebook"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="twitter" href="https://twitter.com">
-                  <img
-                    className="fas fa-lg"
-                    src={twitter}
-                    alt="Twitter"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="instagram" href="https://instagram.com">
-                  <img
-                    src={instagram}
-                    alt="Instagram"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="vimeo" href="https://vimeo.com">
-                  <img
-                    src={vimeo}
-                    alt="Vimeo"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
+      <footer className="bg-grey-500 text-white-500">
+        <div className="container m-auto flex">
+          <div className="content text-center">
+            <img
+              src={logo}
+              alt="SMACK GRC"
+              style={{ width: "14em", height: "10em" }}
+            />
+          </div>
+          <div className="content text-center has-background-black text-white-500">
+            <div className="container has-background-black text-white-500">
+              <div className="columns">
+                <div className="column is-4">
+                  <section className="menu">
+                    <ul className="menu-list">
+                      <li>
+                        <Link to="/" className="text-white-500">
+                          Home
+                        </Link>
+                      </li>
+                      <li>
+                        <Link className="text-white-500" to="/about">
+                          About
+                        </Link>
+                      </li>
+                      <li>
+                        <Link className="text-white-500" to="/products">
+                          Products
+                        </Link>
+                      </li>
+                      <li>
+                        <Link className="text-white-500" to="/contact/examples">
+                          Form Examples
+                        </Link>
+                      </li>
+                      <li>
+                        <a
+                          className="text-white-500"
+                          href="/admin/"
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          Admin
+                        </a>
+                      </li>
+                    </ul>
+                  </section>
+                </div>
+                <div className="column is-4">
+                  <section>
+                    <ul className="menu-list">
+                      <li>
+                        <Link className="text-white-500" to="/blog">
+                          Latest Stories
+                        </Link>
+                      </li>
+                      <li>
+                        <Link className="text-white-500" to="/contact">
+                          Contact
+                        </Link>
+                      </li>
+                    </ul>
+                  </section>
+                </div>
               </div>
             </div>
           </div>
         </div>
+        <div className="container m-auto flex">
+          <a title="facebook" href="https://www.facebook.com/SMACKGRC">
+            <img
+            className="m-1 fill-white"
+              src={facebook}
+              alt="Facebook"
+              style={{ width: "1em", height: "1em" }}
+            />
+          </a>
+          <a title="twitter" href="https://twitter.com/smackgrc">
+            <img
+              className="m-1 fill-white fas fa-lg"
+              src={twitter}
+              alt="Twitter"
+              style={{ width: "1em", height: "1em" }}
+            />
+          </a>
+          <a title="instagram" href="https://www.instagram.com/smackgrc/">
+            <img
+            className="m-1 fill-white"
+              src={instagram}
+              alt="Instagram"
+              style={{ width: "1em", height: "1em" }}
+            />
+          </a>
+          <a title="linkedin" href="https://www.linkedin.com/company/smackgrc/">
+            <img
+            className="m-1 fill-white"
+              src={linkedin}
+              alt="linkedin"
+              style={{ width: "1em", height: "1em" }}
+            />
+          </a>
+        </div>
       </footer>
-    )
+    );
   }
-}
+};
 
-export default Footer
+export default Footer;
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,10 +1,9 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Link, graphql } from 'gatsby'
+import React from "react";
+import PropTypes from "prop-types";
+import { Link, graphql } from "gatsby";
 
-import Layout from '../components/Layout'
-import Features from '../components/Features'
-import BlogRoll from '../components/BlogRoll'
+import Layout from "../components/Layout";
+import Features from "../components/Features";
 
 export const IndexPageTemplate = ({
   image,
@@ -13,38 +12,38 @@ export const IndexPageTemplate = ({
   subheading,
   mainpitch,
   description,
-  intro,
+  intro
 }) => (
   <div>
     <div
-      className="full-width-image margin-top-0"
+      className="mt-0 h-64"
       style={{
         backgroundImage: `url(${
           !!image.childImageSharp ? image.childImageSharp.fluid.src : image
         })`,
         backgroundPosition: `top left`,
-        backgroundAttachment: `fixed`,
+        backgroundAttachment: `fixed`
       }}
     >
       <div
         style={{
-          display: 'flex',
-          height: '150px',
-          lineHeight: '1',
-          justifyContent: 'space-around',
-          alignItems: 'left',
-          flexDirection: 'column',
+          display: "flex",
+          height: "150px",
+          lineHeight: "1",
+          justifyContent: "space-around",
+          alignItems: "left",
+          flexDirection: "column"
         }}
       >
         <h1
           className="font-bold text-2xl"
           style={{
             boxShadow:
-              'rgb(255, 117, 0) 0.5rem 0px 0px, rgb(255, 117, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 117, 0)',
-            color: 'white',
-            lineHeight: '1',
-            padding: '0.25em',
+              "rgb(255, 117, 0) 0.5rem 0px 0px, rgb(255, 117, 0) -0.5rem 0px 0px",
+            backgroundColor: "rgb(255, 117, 0)",
+            color: "white",
+            lineHeight: "1",
+            padding: "0.25em"
           }}
         >
           {title}
@@ -53,11 +52,11 @@ export const IndexPageTemplate = ({
           className="font-bold text-lg"
           style={{
             boxShadow:
-              'rgb(255, 117, 0) 0.5rem 0px 0px, rgb(255, 117, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 117, 0)',
-            color: 'white',
-            lineHeight: '1',
-            padding: '0.25em',
+              "rgb(255, 117, 0) 0.5rem 0px 0px, rgb(255, 117, 0) -0.5rem 0px 0px",
+            backgroundColor: "rgb(255, 117, 0)",
+            color: "white",
+            lineHeight: "1",
+            padding: "0.25em"
           }}
         >
           {subheading}
@@ -80,9 +79,7 @@ export const IndexPageTemplate = ({
                 </div>
                 <div className="columns">
                   <div className="column is-12">
-                    <h3 className="font-semibold is-size-2">
-                      {heading}
-                    </h3>
+                    <h3 className="font-semibold is-size-2">{heading}</h3>
                     <p>{description}</p>
                   </div>
                 </div>
@@ -94,17 +91,6 @@ export const IndexPageTemplate = ({
                     </Link>
                   </div>
                 </div>
-                <div className="column is-12">
-                  <h3 className="font-semibold is-size-2">
-                    Latest stories
-                  </h3>
-                  <BlogRoll />
-                  <div className="column is-12 text-center">
-                    <Link className="btn" to="/blog">
-                      Read more
-                    </Link>
-                  </div>
-                </div>
               </div>
             </div>
           </div>
@@ -112,7 +98,7 @@ export const IndexPageTemplate = ({
       </div>
     </section>
   </div>
-)
+);
 
 IndexPageTemplate.propTypes = {
   image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
@@ -122,12 +108,12 @@ IndexPageTemplate.propTypes = {
   mainpitch: PropTypes.object,
   description: PropTypes.string,
   intro: PropTypes.shape({
-    blurbs: PropTypes.array,
-  }),
-}
+    blurbs: PropTypes.array
+  })
+};
 
 const IndexPage = ({ data }) => {
-  const { frontmatter } = data.markdownRemark
+  const { frontmatter } = data.markdownRemark;
 
   return (
     <Layout>
@@ -141,18 +127,18 @@ const IndexPage = ({ data }) => {
         intro={frontmatter.intro}
       />
     </Layout>
-  )
-}
+  );
+};
 
 IndexPage.propTypes = {
   data: PropTypes.shape({
     markdownRemark: PropTypes.shape({
-      frontmatter: PropTypes.object,
-    }),
-  }),
-}
+      frontmatter: PropTypes.object
+    })
+  })
+};
 
-export default IndexPage
+export default IndexPage;
 
 export const pageQuery = graphql`
   query IndexPageTemplate {
@@ -190,4 +176,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
