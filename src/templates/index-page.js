d288678c2c7f0aa16650bf6316a8a5498a824797
Michael Bonner <michael@redolive.com>
2019-05-21 16:40:06 -0600

    minor styling

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -21,10 +21,16 @@ export const IndexPageTemplate = ({
         <h1 className="font-bold text-4xl">{title}</h1>
         <h3 className="font-bold text-xl">{subheading}</h3>
         <p className="p-8">
-          <Link className=" mx-2 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded uppercase" to="/products">
+          <Link
+            className=" mx-2 bg-transparent hover:bg-yellow-500 text-grey-700 font-semibold hover:text-white-500 py-2 px-4 border border-yellow-500 hover:border-transparent rounded-sm uppercase"
+            to="/products"
+          >
             Features
           </Link>
-          <Link className=" mx-2 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded uppercase" to="/products">
+          <Link
+            className=" mx-2 bg-transparent hover:bg-yellow-500 text-grey-700 font-semibold hover:text-white-500 py-2 px-4 border border-yellow-500 hover:border-transparent rounded-sm uppercase"
+            to="/products"
+          >
             GRC/IRM Requirements
           </Link>
         </p>
@@ -40,27 +46,34 @@ export const IndexPageTemplate = ({
         </div>
       </div>
       <div className="md:w-1/3 content p-4">
-        <div style={{
-          marginTop: '-59px'
-        }}>
+        <div
+          style={{
+            marginTop: "-59px"
+          }}
+        >
           <PreviewCompatibleImage imageInfo={mainpitch} />
         </div>
       </div>
     </div>
     <div className="bg-grey-500 text-white-500 py-24">
-      <div className="container mx-auto flex">
-        <div>
-          <Features gridItems={intro.blurbs} />
-        </div>
-        <div className="container mx-auto p-4 md:w-2/5">
-          <h3 className="font-semibold text-2xl mb-4 text-yellow-500">{heading}</h3>
+      <div className="container mx-auto md:flex md:flex-row-reverse">
+        <div className="container mx-auto p-4 w-full md:w-2/5">
+          <h3 className="font-semibold text-2xl mb-4 text-yellow-500">
+            {heading}
+          </h3>
           <p>{description}</p>
+        </div>
+        <div className="w-full md:w-3/5">
+          <Features gridItems={intro.blurbs} />
         </div>
       </div>
     </div>
     <div className="container mx-auto pt-24 pb-12">
       <div className="text-center">
-        <Link className="py-2 px-4 text-xl border-grey-400 border uppercase" to="/products">
+        <Link
+          className="py-2 px-4 text-xl border-grey-400 border uppercase"
+          to="/products"
+        >
           Request a demo
         </Link>
       </div>
