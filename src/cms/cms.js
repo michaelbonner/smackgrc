4ff5c3af1917b8dd398ce97ac26377969546da3d
Michael Bonner <michael@redolive.com>
2019-06-26 15:53:11 -0600

    solutions page

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,10 +2,10 @@ import CMS from 'netlify-cms'
 
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
-import ProductPagePreview from './preview-templates/ProductPagePreview'
+import SolutionsPagePreview from './preview-templates/SolutionsPagePreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
 
 CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
-CMS.registerPreviewTemplate('products', ProductPagePreview)
+CMS.registerPreviewTemplate('solutions', SolutionsPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
