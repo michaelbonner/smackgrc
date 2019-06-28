a1d36e5fa26903321696e233cce22e81095425e6
Michael Bonner <michael@redolive.com>
2019-06-28 14:13:14 -0600

    requirements template

---
+++ src/cms/preview-templates/RequirementsPagePreview.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { RequirementsPageTemplate } from '../../templates/requirements-page'
+
+const RequirementsPagePreview = ({ entry, getAsset }) => {
+  const entryBlurbs = entry.getIn(['data', 'intro', 'blurbs'])
+  const blurbs = entryBlurbs ? entryBlurbs.toJS() : []
+
+  return (
+    <RequirementsPageTemplate
+      image={entry.getIn(['data', 'image'])}
+      title={entry.getIn(['data', 'title'])}
+      heading={entry.getIn(['data', 'heading'])}
+      description={entry.getIn(['data', 'description'])}
+      intro={{ blurbs }}
+      main={{
+        heading: entry.getIn(['data', 'main', 'heading']),
+        description: entry.getIn(['data', 'main', 'description']),
+      }}
+      fullImage={entry.getIn(['data', 'full_image'])}
+    />
+  )
+}
+
+RequirementsPagePreview.propTypes = {
+  entry: PropTypes.shape({
+    getIn: PropTypes.func,
+  }),
+  getAsset: PropTypes.func,
+}
+
+export default RequirementsPagePreview
