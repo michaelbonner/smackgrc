cb92228008d0ebb602a376e25727c564eabc894d
Michael Bonner <michael@redolive.com>
2019-07-01 13:39:15 -0600

    allow editing buttons on index page

--- src/cms/preview-templates/IndexPagePreview.js
+++ src/cms/preview-templates/IndexPagePreview.js
@@ -12,6 +12,7 @@ const IndexPagePreview = ({ entry, getAsset }) => {
         title={data.title}
         heading={data.heading}
         subheading={data.subheading}
+        button={data.button}
         description={data.description}
         intro={data.intro || { blurbs: [] }}
         mainpitch={data.mainpitch || {}}
